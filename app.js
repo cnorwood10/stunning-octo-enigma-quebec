e57789bc2f52e73f8c406e023b7b84ad098de9cc
Colby Norwood <colbynorwood@Colbys-MacBook-Pro.local>
2024-03-02 22:54:08 -0600

    connected to db, showing results

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser =  require("body-parser");
+
 
 // set the view engine to ejs
 let path = require('path');
@@ -21,38 +23,44 @@ const client = new MongoClient(process.env.URI, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
-
-
-
-    return result;
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+async function getVehicleData() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      // await client.db("admin").command({ ping: 1 });
+      const result = await client.db("quebec-database").collection("quebec").find().toArray();
+      console.log("mongo call await inside f/n: ", result);
+      return result; 
+    } 
+    catch(err) {
+      console.log("getVehicleData() error: ", err);
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      //await client.close();
+    }
   }
-}
 
 app.get('/', async (req,res) => {
 
-  let dbResult = await run();
+  let result = await getVehicleData();
 
-  console.log("myResults:", dbResult);
+  console.log("myResults:", result);
 
   res.render('index', {
-    myView: dbResult
+    pageTitle: "Vehicle Data",
+    vehicleData: result
 
   });
 
 });
 
+app.get('/add', async (req,res) => {
+    
+
+});
+
 app.listen(port, () => {
-  console.log(`papa app listening on port ${port}`)
+  console.log(`quebec app listening on port ${port}`)
 })
