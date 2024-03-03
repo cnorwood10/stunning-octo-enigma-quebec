433ed78ca64f23e0443591bc165d0a317628b3d9
Colby Norwood <colbynorwood@Colbys-MacBook-Pro.local>
2024-03-03 02:45:13 -0600

    format code

--- app.js
+++ app.js
@@ -21,44 +21,44 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function connectVehicleData() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      // await client.db("admin").command({ ping: 1 });
-      const result = await client.db("quebec-database").collection("quebec").find().toArray();
-      console.log("mongo call await inside f/n: ", result);
-      return result; 
-    } 
-    catch(err) {
-      console.log("getVehicleData() error: ", err);
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        const result = await client.db("quebec-database").collection("quebec").find().toArray();
+        console.log("mongo call await inside f/n: ", result);
+        return result;
+    }
+    catch (err) {
+        console.log("getVehicleData() error: ", err);
     }
     finally {
-      // Ensures that the client will close when you finish/error
-      //await client.close();
+        // Ensures that the client will close when you finish/error
+        //await client.close();
     }
-  }
+}
 
 // Read from Database
-app.get('/', async (req,res) => {
+app.get('/', async (req, res) => {
 
-  let result = await connectVehicleData();
+    let result = await connectVehicleData();
 
-  console.log("myResults:", result);
+    console.log("myResults:", result);
 
-  res.render('index', {
-    pageTitle: "Vehicle Data",
-    vehicleData: result
+    res.render('index', {
+        pageTitle: "Vehicle Data",
+        vehicleData: result
 
-  });
+    });
 
 });
 
@@ -66,26 +66,26 @@ app.get('/', async (req,res) => {
 app.post('/addRegistration', async (req, res) => {
 
     try {
-      // console.log("req.body: ", req.body) 
-      client.connect; 
-      const collection = client.db("quebec-database").collection("quebec");
-      
-      //draws from body parser 
-      console.log(req.body);
-      
-      await collection.insertOne(req.body);
-        
-  
-      res.redirect('/');
+        // console.log("req.body: ", req.body) 
+        client.connect;
+        const collection = client.db("quebec-database").collection("quebec");
+
+        //draws from body parser 
+        console.log(req.body);
+
+        await collection.insertOne(req.body);
+
+
+        res.redirect('/');
     }
-    catch(err){
-      console.log(err)
+    catch (err) {
+        console.log(err)
     }
-    finally{
-     // client.close()
+    finally {
+        // client.close()
     }
-  
-  });
+
+});
 
 
 
@@ -94,52 +94,52 @@ app.post('/updateVehicleData', async (req, res) => {
 
     try {
 
-      console.log("body: ", req.body);
-      
-      client.connect; 
-      const collection = client.db("quebec-database").collection("quebec");
-      let result = await collection.findOneAndUpdate( 
-        {_id: new ObjectId(req.body.id)}, 
-        {$set: {name: req.body.name ,vehicleMileage: req.body.vehicleMileage}}
+        console.log("body: ", req.body);
+
+        client.connect;
+        const collection = client.db("quebec-database").collection("quebec");
+        let result = await collection.findOneAndUpdate(
+            { _id: new ObjectId(req.body.id) },
+            { $set: { name: req.body.name, vehicleMileage: req.body.vehicleMileage } }
         )
 
-      .then(result => {
-        console.log(result); 
-        res.redirect('/');
-      })
-      .catch(error => console.error(error))
+            .then(result => {
+                console.log(result);
+                res.redirect('/');
+            })
+            .catch(error => console.error(error))
     }
-    finally{
-      //client.close()
+    finally {
+        //client.close()
     }
-  
-  });
 
-  // Delete from Database
-  app.post('/deleteVehicleData', async (req, res) => {
+});
+
+// Delete from Database
+app.post('/deleteVehicleData', async (req, res) => {
 
     try {
-        console.log("body: ", req.body); 
-      
-      client.connect; 
-      const collection = client.db("quebec-database").collection("quebec");
-      let result = await collection.findOneAndDelete( 
-        {
-          "_id": new ObjectId(req.body.id)
-        }
-      )
-      .then(result => {
-        console.log(result); 
-        res.redirect('/');
-      })
-      .catch(error => console.error(error))
+        console.log("body: ", req.body);
+
+        client.connect;
+        const collection = client.db("quebec-database").collection("quebec");
+        let result = await collection.findOneAndDelete(
+            {
+                "_id": new ObjectId(req.body.id)
+            }
+        )
+            .then(result => {
+                console.log(result);
+                res.redirect('/');
+            })
+            .catch(error => console.error(error))
     }
-    finally{
-      //client.close()
+    finally {
+        //client.close()
     }
-  
-  })
+
+})
 
 app.listen(port, () => {
-  console.log(`quebec app listening on port ${port}`)
+    console.log(`quebec app listening on port ${port}`)
 })
